9f3c6789087da253141b0ece7852daa1dc22ceed
brayenid <bfenomenal@gmail.com>
2022-11-22 12:39:55 +0800

    Add redirect file

--- brayenweb/src/router/index.js
+++ brayenweb/src/router/index.js
@@ -15,6 +15,10 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: 404
     }
   ]
 })
