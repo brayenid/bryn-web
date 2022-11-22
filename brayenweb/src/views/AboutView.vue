832964f9e4e73bb75e5028931cd85f7a15416d4f
brayenid <bfenomenal@gmail.com>
2022-11-22 23:11:35 +0800

    Edit about view

--- brayenweb/src/views/AboutView.vue
+++ brayenweb/src/views/AboutView.vue
@@ -1,12 +1,14 @@
 <script setup>
-import IconLeftVue from '../components/icons/IconLeft.vue'
+import IconExitVue from '../components/icons/IconExit.vue'
 import { RouterLink } from 'vue-router'
 </script>
 <template>
   <div class="aboutContainer">
-    <RouterLink to="/"><IconLeftVue /></RouterLink>
     <div class="aboutMe">
-      <h1>About Me</h1>
+      <div class="exit">
+        <RouterLink class="exit" to="/" aria-label="Homepage"><IconExitVue /></RouterLink>
+      </div>
+      <h1>Hello World!</h1>
       <p>
         I don't know if anyone will ever open this page, but hey. Once again, my name is Brayen Luhat you can call me Brayen. I live in a small district in East Kalimantan (Kalimantan), West Kutai, Indonesia. I am an independent Web
         Developer student and always will be. My main programming language is Javascript (JS). I usually work as a fullstack JS developer. <br /><br />
@@ -29,26 +31,17 @@ import { RouterLink } from 'vue-router'
 .aboutMe {
   max-width: 700px;
   margin: auto;
-  padding: 1rem;
+  padding: 0 1rem 1rem;
   box-sizing: border-box;
   min-height: 100vh;
+  overflow: hidden;
 }
 .aboutMe h1 {
   font-size: 2rem;
   font-weight: bold;
-  text-transform: uppercase;
   margin-bottom: 1rem;
   position: relative;
   padding: 0.7rem 0;
-  width: max-content;
-}
-.aboutMe h1::before {
-  content: '';
-  width: 100%;
-  height: 4px;
-  background-color: var(--mainColor);
-  position: absolute;
-  bottom: 0;
 }
 .aboutMe p {
   font-size: 1.1rem;
@@ -77,8 +70,17 @@ import { RouterLink } from 'vue-router'
 .aboutMe a {
   padding: 3rem;
 }
+.exit {
+  text-align: right;
+  transform: translateX(3rem);
+  padding: 1rem 0;
+}
+.exit a:hover {
+  color: var(--mainColor);
+}
 a {
   color: var(--backgroundColor);
+  transition: 0.2s ease-in-out;
 }
 a:active {
   color: var(--mainColor);
